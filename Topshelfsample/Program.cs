9ce2244720c0f7c864d574a75ffa20c6fad936d3
Kumaraguru s <kumaraguru.s@mphasis.com>
2023-09-06 08:52:21 +0530

    Add project files.

---
+++ Topshelfsample/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using Topshelf;
+using Topshelfsample;
+
+namespace Testservice1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var code = HostFactory.Run(x =>
+                {
+                    x.Service<TestService1>(s => {
+                        s.ConstructUsing(Service => new TestService1());
+                        s.WhenStarted(Service => Service.Start());
+                        s.WhenStopped(Service => Service.Stop());
+                    }); 
+                    x.RunAsLocalSystem();
+                    x.SetServiceName("TestService1");
+                    x.SetDisplayName("TestService1");
+                    x.SetDescription("this is an important service dont stop");
+
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Topshelfsample/TestService1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Timers;
+using System.IO;
+
+namespace Topshelfsample
+{
+    class TestService1
+    {
+        private readonly System.Timers.Timer _timer;
+
+        public TestService1()
+        {
+            _timer = new System.Timers.Timer(2000) { AutoReset = true };
+            _timer.Elapsed += ExecuteService;
+        }
+        private void ExecuteService(object sender, ElapsedEventArgs ev)
+        {
+            string[] files = System.IO.Directory.GetFiles(@"in");
+            foreach (string filepath in files)
+            {
+                string filename = Path.GetFileName(filepath);
+                System.IO.File.Copy(@"in/" + filename, @"out/" + filename);
+                System.IO.File.Delete(@"in/" + filename);
+            }
+            try
+            {
+                _timer.Stop();
+            }
+            catch(Exception e) {
+                Console.WriteLine("something went rong:" +e.Message);
+            }
+            finally { _timer.Start(); }
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
